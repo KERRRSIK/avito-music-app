66994ba77c4cfab5cbbf8429f1a0e835e8b66925
KERRRSIK <99367490+KERRRSIK@users.noreply.github.com>
2025-02-17 00:59:52 +0300

    Delete app directory

--- app/src/main/java/com/example/avito_audio_player/data/model/Chart.kt
+++
@@ -1,5 +0,0 @@
-package com.example.avito_audio_player.data.model
-
-data class Chart(
-    val tracks: DeezerResponse
-)
--- app/src/main/java/com/example/avito_audio_player/data/model/DeezerResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.avito_audio_player.data.model
-
-data class DeezerResponse(
-    val `data`: List<TrackAPI>,
-    val next: String,
-    val total: Int
-)
